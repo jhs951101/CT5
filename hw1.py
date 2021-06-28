60369c83ce71e162cda575d31420b1ad11f361cf
spg1101 <sharping@SharPingui-MacBookPro.local>
2021-06-28 19:20:54 +0900

    m

---
+++ hw1.py
@@ -0,0 +1,13 @@
+﻿name = input('city name : ')
+str1 = ''
+
+if name == '서울':
+    str1 = name + ' : 605㎢'
+elif name == '파리':
+    str1 = name + ' : 105㎢'
+elif name == '애든버러':
+    str1 = name + ' : 264㎢'
+else:
+    str1 = 'Sorry'
+
+print(str1)
---
+++ hw2.py
@@ -0,0 +1,16 @@
+﻿import datetime
+
+number = int( input('차량번호 입력 : ') )
+
+day = datetime.datetime.now().day
+str1 = ''
+
+if day%2 == 0:
+    str1 = '짝수날'
+else:
+    str1 = '홀수날'
+
+if (day%2 == 0 and number%2 == 0) or (day%2 == 1 and number%2 == 1):
+    print('오늘은 %d로 %s로 주차가능합니다.' %(day, str1))
+else:
+    print('오늘은 %d로 %s로 주차 불가능합니다.' %(day, str1))
